a8b1baaa4ae9dab70f9a76aa1a3510a307a5032a
daniellehloura <daniellehloura@gmail.com>
2025-06-16 14:45:57 +0000

    widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,14 @@
-// This is a basic Flutter widget test.
+// This is a basic Flutter widgetflutter_test/flutter_test.dart'; test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'padart
+ckage:flutter/material.dart';
+import """
+package:flutter_test/flutter_test.dart""";
 
 import 'package:myapp/main.dart';
 
